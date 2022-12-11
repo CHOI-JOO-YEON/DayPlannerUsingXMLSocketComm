db1698c345c867ffaad9a53b54ddd9c4af2aa261
연주 <50358956+Yeon-jooKim@users.noreply.github.com>
2022-12-12 02:36:10 +0900

    test: id 중복 확인 테스트

--- src/test/UserTest.java
+++ src/test/UserTest.java
@@ -2,20 +2,29 @@ package test;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import server.User;
-import org.assertj.core.api.Assertions;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class UserTest {
     User user = new User();
     @Test
     void 로그인_테스트(){
-        Assertions.assertThat(user.isLogin("Joo", "1234")).isTrue();
+        assertThat(user.isLogin("Joo", "1234")).isTrue();
     }
 
     @Test
     void 로그인_실패_테스트() {
-        Assertions.assertThat(user.isLogin("Joo", "5678")).isFalse();
+        assertThat(user.isLogin("Joo", "5678")).isFalse();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"Joo:false","Kim:true"}, delimiter = ':')
+    void ID_중복_테스트(String id, boolean answer) {
+        assertThat(user.isNonDuplicateId(id)).isEqualTo(answer);
     }
 
 }
